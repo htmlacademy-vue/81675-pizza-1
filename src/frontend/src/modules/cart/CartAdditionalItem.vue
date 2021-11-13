b663fca3ec88738eb9185f952644ff6fa7c435fd
karataev <vfl45es1>
2021-11-13 11:03:58 +0700

    Разбил страницу корзины на компоненты

---
+++ src/frontend/src/modules/cart/CartAdditionalItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <li class="additional-list__item sheet">
+    <p class="additional-list__description">
+      <img :src="item.image" width="39" height="60" :alt="item.name" />
+      <span>{{ item.name }}</span>
+    </p>
+
+    <div class="additional-list__wrapper">
+      <div class="counter additional-list__counter">
+        <button
+          type="button"
+          class="counter__button counter__button--minus"
+          :disabled="isAddDisabled"
+          @click="item.amount--"
+        >
+          <span class="visually-hidden">Меньше</span>
+        </button>
+        <input
+          type="text"
+          name="counter"
+          class="counter__input"
+          :value="item.amount"
+        />
+        <button
+          type="button"
+          class="counter__button counter__button--plus counter__button--orange"
+          :disabled="isRemoveDisabled"
+          @click="item.amount++"
+        >
+          <span class="visually-hidden">Больше</span>
+        </button>
+      </div>
+
+      <div class="additional-list__price">
+        <b>× {{ item.price }} ₽</b>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "CartAdditionalItem",
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    isAddDisabled() {
+      return this.item.amount <= 0;
+    },
+    isRemoveDisabled() {
+      return this.item.amount >= 10;
+    },
+  },
+};
+</script>
--- src/frontend/src/store/cart.js
+++ src/frontend/src/store/cart.js
@@ -1,7 +1,28 @@
+import miscData from "@/static/misc.json";
+import colaImg from "@/assets/img/cola.svg";
+import sauceImg from "@/assets/img/sauce.svg";
+import potatoImg from "@/assets/img/potato.svg";
+
+const images = [
+  { id: 1, image: colaImg },
+  { id: 2, image: sauceImg },
+  { id: 3, image: potatoImg },
+];
+
+const normalizedMiscData = miscData.map((item) => {
+  const img = images.find((imgItem) => imgItem.id === item.id);
+  return {
+    ...item,
+    image: img.image,
+    amount: 0,
+  };
+});
+
 export default {
   namespaced: true,
   state: {
     cart: [],
+    additional: normalizedMiscData,
   },
   mutations: {
     addToCart(state, payload) {
@@ -9,7 +30,12 @@ export default {
     },
   },
   getters: {
-    cartTotalPrice(state) {
+    additionalPrice(state) {
+      return state.additional.reduce((acc, item) => {
+        return acc + item.price * item.amount;
+      }, 0);
+    },
+    cartTotalPrice(state, getters) {
       const pizzaPrice = (pizza) => {
         const { dough, size, sauce, ingredients } = pizza;
         const ingredientsPrice = ingredients.reduce((acc, item) => {
@@ -18,7 +44,11 @@ export default {
         return size.multiplier * (dough.price + sauce.price + ingredientsPrice);
       };
 
-      return state.cart.reduce((acc, pizza) => acc + pizzaPrice(pizza), 0);
+      const pizzasPrice = state.cart.reduce(
+        (acc, pizza) => acc + pizzaPrice(pizza),
+        0
+      );
+      return pizzasPrice + getters.additionalPrice;
     },
   },
 };
