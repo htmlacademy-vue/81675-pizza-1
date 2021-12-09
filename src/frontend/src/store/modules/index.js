1fb808d0c779f1dcb892bde29168ece46c671915
karataev <eugene.karataev@gmail.com>
2021-12-09 10:04:01 +0700

    Рефакторинг структуры папок стора

--- src/frontend/src/store/modules/index.js
+++ src/frontend/src/store/modules/index.js
@@ -1,9 +1,9 @@
-import auth from "@/store/auth";
-import builder from "@/store/builder";
-import cart from "@/store/cart";
-import orders from "@/store/orders";
-import address from "@/store/address";
-import publicModule from "@/store/public";
+import auth from "@/store/modules/auth";
+import builder from "@/store/modules/builder";
+import cart from "@/store/modules/cart";
+import orders from "@/store/modules/orders";
+import address from "@/store/modules/address";
+import publicModule from "@/store/modules/public";
 
 export default {
   Auth: auth,
